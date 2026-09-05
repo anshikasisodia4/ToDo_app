4db24ac30ab816387fa12ccaff650ca9658d4cc7
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-05 10:57:00 +0530

    task_card

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,11 +1,41 @@
-class Task {
-  String title;
-  String description;
-  bool isCompleted;
+import 'package:flutter/material.dart';
+import '../models/task.dart';
 
-  Task({
-    required this.title,
-    this.description = '',
-    this.isCompleted = false,
+class TaskCard extends StatelessWidget {
+  final Task task;
+  final VoidCallback onDelete;
+  final VoidCallback onToggle;
+
+  const TaskCard({
+    super.key,
+    required this.task,
+    required this.onDelete,
+    required this.onToggle,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        leading: Checkbox(
+          value: task.isCompleted,
+          onChanged: (_) => onToggle(),
+        ),
+        title: Text(
+          task.title,
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w500,
+            decoration:
+                task.isCompleted ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.delete_outline),
+          onPressed: onDelete,
+        ),
+      ),
+    );
+  }
 }
